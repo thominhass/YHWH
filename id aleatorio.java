20b9884bde9e680b9f5bffafc48d2b6274e143e0
thominhass <141268228+thominhass@users.noreply.github.com>
2023-08-09 08:05:25 -0300

    Update id aleatorio.java

--- id aleatorio.java
+++ id aleatorio.java
@@ -1,1 +1,11 @@
+import java.util.Random;
 
+class CalcularHoras{
+    public static void main(String[] args) {
+     
+        //cria um objeto que sera utilizado para criar o id de consulta 
+        Random consulta = new Random()
+        
+        
+    }
+}
